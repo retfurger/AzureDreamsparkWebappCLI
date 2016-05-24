0f34f45941c6c72664ed21c077a9b08e92fa5f16
reto_furger <ret.furger@gmail.com>
2016-05-24 17:52:44 +0200

    lets go

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ var http = require('http');
 http.createServer(function (req, res) {
     console.log('Got request for ' + req.url);
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>Hello Code and Azure Web Apps!</h1>');
+    res.end('<h1>Hello Cloud from CLI!</h1>');
 }).listen(process.env.PORT);
